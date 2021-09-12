29541f3372991e59e3251b280f3d6ff6f07bffdf
pranavkashyap06 <35664868+pranavkashyap06@users.noreply.github.com>
2021-09-12 21:01:12 +0530

    Add files via upload

--- PreProcess_Files/__init__.py
+++ PreProcess_Files/__init__.py
@@ -1,48 +1,49 @@
-from Preprocess_text_file_Pranav import Utils
+from Preprocess_text_file_Pranav import utils
 
 __version__='0.0.1'
 
 def get_wordscount(x):
-	returns Utils._get_wordscount(x):
+	returns utils._get_wordscount(x):
 
 def get_charcounts(x):
-	returns Utils._get_charcounts(x):
+	returns utils._get_charcounts(x):
 
 def get_stopword(x):
-	returns Utils._get_stopword(x):
+	returns utils._get_stopword(x):
 
 def get_averagecount(x):
-	returns Utils._get_averagecount(x):
+	returns utils._get_averagecount(x):
 
 def get_hashtagcounts(x):
-	returns Utils._get_hashtagcounts(x):
+	returns utils._get_hashtagcounts(x):
 
 def get_mentioncounts(x):
-	returns Utils._get_mentioncounts(x):
+	returns utils._get_mentioncounts(x):
 
 def get_digitcountst(x):
-	returns Utils._get_digitcounts(x):
+	returns utils._get_digitcounts(x):
 
 def get_uppercount(x):
-	returns Utils._get_uppercount(x):
+	returns utils._get_uppercount(x):
 
 def get_emails(x):
-	returns Utils._get_emails(x):
+	returns utils._get_emails(x):
 
 def remove_emails(x):
-	returns Utils._remove_emails(x):
+	returns utils._remove_emails(x):
 
 def remove_rt(x):
-	returns Utils._remove_rt(x):
+	returns utils._remove_rt(x):
 
 def remove_specialcharacter(x):
-	returns Utils._remove_specialcharacter(x):
+	returns utils._remove_specialcharacter(x):
 
 def remove_html_tags(x):
-	returns Utils._remove_html_tags(x):
+	returns utils._remove_html_tags(x):
 
 def remove_accentedchar(x):
-	returns Utils._remove_accentedchar(x):
+	returns utils._remove_accentedchar(x):
 
 def spelling_correction(x):
-	returns Utils._spelling_correction(x):
+	returns utils._spelling_correction(x):
+
